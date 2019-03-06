5a03573f48eb33d57f63a41998769f847d36a83f
Huang Yunkun <htynkn@gmail.com>
2019-03-06 19:55:18 +0800

    modify test script

--- dubbo-admin/latest/test.sh
+++ dubbo-admin/latest/test.sh
@@ -1,11 +1,20 @@
-sleep 60
+LOOP_SIZE=60
+i=0
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://admin:8080/swagger-resources)
+while [[ $i -lt LOOP_SIZE ]]; do
+	status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://admin:8080/swagger-resources)
 
-if [[ "$status_code" -ne 200 ]] ; then
-  echo "Tests passed!"
-  exit 0
-else
-  echo "Tests failed!"
-  exit 1
-fi
+  if [[ "$status_code" -eq 200 ]] ; then
+    echo "Tests passed!"
+    exit 0
+  else
+    curl -v http://admin:8080/swagger-resources
+    echo "status is incorrect, waiting for next turn"
+  fi
+	sleep 5
+	i=$i+1
+done
+
+echo "Tests failed!"
+exit 1
+
