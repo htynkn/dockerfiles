88d07cfe8e9f863588ae4a41e459e61f7ac67c41
Huang Yunkun <htynkn@gmail.com>
2019-03-07 11:44:39 +0800

    fix health script

--- dubbo-admin/latest/health.sh
+++ dubbo-admin/latest/health.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/swagger-resources)
+LOOP_SIZE=10
+i=0
 
-if [[ "$status_code" -eq 200 ]] ; then
-  exit 0
-else
-  curl -v http://admin:8080/swagger-resources
-  exit 1
-fi
+while [[ $i -lt LOOP_SIZE ]]; do
+	status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/swagger-resources)
+
+  if [[ "$status_code" -eq 200 ]] ; then
+    echo "health check passed!"
+    exit 0
+  else
+    curl -v http://localhost:8080/swagger-resources
+    echo "status is incorrect, waiting for next turn"
+  fi
+	sleep 10
+	i=$i+1
+done
+
+echo "health check failed!"
+exit 1
+
