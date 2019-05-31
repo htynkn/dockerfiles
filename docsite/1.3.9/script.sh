171304f93132ad35643c1aa4eacfd8456ea41adc
Huang Yunkun <htynkn@gmail.com>
2019-05-31 17:20:58 +0800

    update

--- docsite/1.3.9/script.sh
+++ docsite/1.3.9/script.sh
@@ -3,7 +3,12 @@
 echo "Start clone $1"
 git clone --branch $2 $1 source
 cd source
+
+ehco "Install npm depdenency"
+npm i
 docsite build
+
+echo "Start push changes back"
 git add .
 COMMIT_ID=`git rev-parse HEAD`
 git commit -m "generate from $COMMIT_ID"
